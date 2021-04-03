73584e58b59f1cf30fc77f657e86b70d58966989
Quang Tran <qtran1997@gmail.com>
2021-04-03 15:20:02 -0700

    feat(ios): set up xcode project

---
+++ ios/Book Club App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Book Club App
+//
+//  Created by Quang Tran on 4/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
