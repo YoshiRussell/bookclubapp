309c522cd3a1180eff3ed0df746b6b2fb9e5aea4
Edwin <edwin.h.do@gmail.com>
2021-04-02 20:54:11 -0700

    initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func newRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler).Methods("GET")
+	return r
+}
+
+func main() {
+	r := newRouter()
+	http.ListenAndServe(":8080", r)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the epic fortnite book club app!\n")
+}
---
+++ main_test.go
@@ -0,0 +1,31 @@
+//main_test.go
+
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	recorder := httptest.NewRecorder()
+	hf := http.HandlerFunc(handler)
+	hf.ServeHTTP(recorder, req)
+
+	if status := recorder.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := "Welcome to the epic fortnite book club app!\n"
+	actual := recorder.Body.String()
+	if actual != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", actual, expected)
+	}
+}
