37399c635fa3ddac8ed8d1c990c7ddde7af9ea43
Edwin <edwin.h.do@gmail.com>
2021-04-04 22:28:53 -0700

    refactor route handler into separate file, update go.mod for new repo

--- main.go
+++ main.go
@@ -1,21 +1,10 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"github.com/YoshiRussell/bookclubapp/server/routes"
 )
 
-func SetupRouter() *gin.Engine {
-	r := gin.Default()
-	r.GET("/epic", epicHandler)
-	return r
-}
 func main() {
-	router := SetupRouter()
+	router := routes.SetupRouter()
 	router.Run()
-}
-
-func epicHandler(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "fortnite",
-	})
 }
