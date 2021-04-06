eaf267fa7432d9f8d7ac82e1aa58be71b838069a
Calvin Nguy <calvinnguy96@gmail.com>
2021-04-05 18:51:47 -0700

    Created basic homepage using React

---
+++ client/src/components/navbar/MenuItems.js
@@ -0,0 +1,19 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Profile',
+        url: '#',
+        cName: 'nav-links'
+    }
+    /** ,
+    {
+        title: 'Sign Up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+    */
+]
